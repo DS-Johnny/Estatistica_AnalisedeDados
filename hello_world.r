fb4e9aa13a51d1b3489cfa95937ae991809747da
Johnny Matos <128853698+DS-Johnny@users.noreply.github.com>
2024-07-15 09:58:55 -0300

    1 cicle exercises

--- hello_world.r
+++ hello_world.r
@@ -1,3 +1,12 @@
 print("Hello, World!")
 
 print('Estatística')
+
+
+file_path <- "datasets/olist_order_items_dataset.csv"
+
+dados <- read.csv(file_path, header = TRUE, sep = ",", stringsAsFactors = FALSE)
+
+df <- data.frame(dados)
+
+print(head(df))
